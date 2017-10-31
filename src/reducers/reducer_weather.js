c6a5a23412d8d5578b7a6b04f6e492f72eaa6af8
Koji Nakagawa <lightupthesky0627@gmail.com>
2017-10-30 22:10:05 -0700

    add weather-list.js

--- src/reducers/reducer_weather.js
+++ src/reducers/reducer_weather.js
@@ -3,8 +3,7 @@ import { FETCH_WEATHER } from '../actions/index';
 export default function(state = [], action) {
   switch (action.type) {
     case FETCH_WEATHER:
-      return state.concat([action.payload.data]);
-      // OR return[ action.payload.data, ...state];
+      return[ action.payload.data, ...state];
       //concat doesn't change original array (state) so its not break rule; manipulating state directlly
   }
   return state;
